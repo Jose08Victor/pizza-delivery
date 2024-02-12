848e244d9eb7272f59b0bebabbbd344fb0e49c43
José Victor <jose08.victor@gmail.com>
2024-02-12 17:24:30 -0300

    added footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,35 @@
+import "./style.scss"
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h3>Contato</h3>
+
+                <p>(55) 98888-0000</p>
+
+                <p>(55) 97777-0000</p>
+            </div>
+
+            <div>
+                <h3>Horário</h3>
+
+                <p>
+                    <strong>Segunda – Sábado</strong>
+                    <br /> 9am – 7pm
+                </p>
+
+                <p>
+                    <strong>Sunday</strong>
+                    <br /> 10am – 6pm
+                </p>
+            </div>
+
+            <div>
+                <h3>Endereço</h3>
+
+                <p>121 Rock Sreet, 21 Avenue, New York</p>
+            </div>
+        </footer>
+    )
+}
