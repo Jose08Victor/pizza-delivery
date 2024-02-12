1ef640b0bbf460767b82e2082bdbe3ad1504b7ce
José Victor <jose08.victor@gmail.com>
2024-02-12 17:24:49 -0300

    Update header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { CartProductsCounter } from '../cart-products-counter';
-import erro from '../../assets/pizza-delivery-logo.png';
 import "./style.scss"
 
 export const Header = () => {
@@ -8,9 +7,9 @@ export const Header = () => {
     <header>
       <div>
         <Link to="/pizza-delivery">
-          {/* <img src="https://raw.githubusercontent.com/Jose08Victor/pizza-delivery/main/src/assets/pizza-delivery-logo.png" alt="Pizza Delivery Logo" /> */}
-          <img src={erro} alt="teste" />
+          <img src="https://raw.githubusercontent.com/Jose08Victor/pizza-delivery/main/src/assets/pizza-delivery-logo.png" alt="Pizza Delivery Logo" />
         </Link>
+
         <CartProductsCounter />
       </div>
     </header>
