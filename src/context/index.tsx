dca2e013626ca8f79d066ea0c9ee7dd278c28b1b
José Victor <jose08.victor@gmail.com>
2024-02-16 19:51:23 -0300

    Update: useCallback added

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useState } from "react";
 import productsJson from "../json/products.json";
 
 interface IpizzaData {
@@ -14,7 +14,7 @@ interface IpizzaData {
 interface IAppContext {
     pizzaData: IpizzaData[];
     handleQuantityChange(product: IpizzaData, type: "increase" | "decrease"): void;
-    handleListChange(product: IpizzaData,  type: "add" | "remove"): void;
+    handleListChange(product: IpizzaData, type: "add" | "remove"): void;
 }
 
 export const AppContext = createContext<IAppContext>({
@@ -27,11 +27,11 @@ export const AppProvider = ({ children }: { children: JSX.Element }) => {
 
     const [pizzaData, setPizzaData] = useState<IpizzaData[]>(productsJson.data);
 
-    const handleQuantityChange = (product: IpizzaData, type: "increase" | "decrease") => {
+    const handleQuantityChange = useCallback((product: IpizzaData, type: "increase" | "decrease") => {
         const newData = pizzaData.map(item => {
             if (item.id === product.id) {
                 let newQuantity = item.quantity;
-                
+
                 if (type === "increase") newQuantity += 1;
 
                 else if (type === "decrease" && item.quantity > 1) newQuantity -= 1;
@@ -41,9 +41,9 @@ export const AppProvider = ({ children }: { children: JSX.Element }) => {
             return item;
         });
         setPizzaData(newData);
-    };
+    }, [pizzaData]);
 
-    function handleListChange(product: IpizzaData,  type: "add" | "remove") {
+    const handleListChange = useCallback((product: IpizzaData, type: "add" | "remove") => {
         const newData = pizzaData.map(item => {
             if (item.id === product.id && type === "add") return { ...item, onAList: true };
 
@@ -52,7 +52,7 @@ export const AppProvider = ({ children }: { children: JSX.Element }) => {
             return { ...item }
         });
         setPizzaData(newData);
-    }
+    }, [pizzaData]);
 
     return (
         <AppContext.Provider value={{ pizzaData, handleQuantityChange, handleListChange }}>
